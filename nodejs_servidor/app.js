455b85ed15d0f0ae35f1c724d1853cb15379ecff
PatricioGitHub1 <rojas_patricio_github@gmx.es>
2024-02-20 17:33:13 +0100

    modified npm packages

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer');
+const fetch = require('cross-fetch');
 const fs = require('fs');
 const url = require('url');
 const { isNullOrUndefined } = require('util');
