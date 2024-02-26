0fda411afd09346abb99baac9ce37e0966739ee9
root <root@ams24.ieti.local>
2024-02-26 15:09:52 +0000

    sprint 2 modificaciones

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -68,7 +68,28 @@ app.post('/api/user/register', async (req, res) => {
 // Endpoint validar usuari
 app.post('/api/user/validate', async (req, res) => {
   console.log("En validar usuari");
-  
+
+  const textPost = req.body;
+  let objRequest = {telefon: textPost.phone, codi_validacio: textPost.number};
+  console.log("Body to DBAPI = "+objRequest);
+
+  try {
+    const response = await fetch('http://localhost:8080/api/usuaris/validar',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(objRequest)
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+
+  } catch (error) {
+    console.error(error);
+  }
 })
 
 // Endpoint descripcio imatges
@@ -143,6 +164,7 @@ app.post('/api/maria/image', upload.array('file'), async (req, res) => {
                 const jsonData = JSON.parse(element);
                 //console.log(jsonData.response);
                 res.write(jsonData.response);
+
               }
             } else {
               const jsonData = JSON.parse(jsonString);
